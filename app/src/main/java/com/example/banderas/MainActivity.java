3531535f34a73e58f3e0d8d7e67bd56ebe9284fb
egakipedia <juancarlos.roldan@egaclm.com>
2023-04-25 23:28:10 +0200

    Añado el método Comprobar

--- app/src/main/java/com/example/banderas/MainActivity.java
+++ app/src/main/java/com/example/banderas/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.banderas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView img_bandera;
     private RadioButton rb1, rb2, rb3;
+    private TextView respuesta;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -26,15 +30,17 @@ public class MainActivity extends AppCompatActivity {
         rb1 = findViewById(R.id.rb_1);
         rb2 = findViewById(R.id.rb_2);
         rb3 = findViewById(R.id.rb_3);
+        respuesta = findViewById(R.id.tv_respuesta);
 
         ArrayList<String> europa_nombre = new ArrayList<String>();
+
+        int europa_bandera [] = {R.drawable.spain, R.drawable.france, R.drawable.germany, R.drawable.denmark, R.drawable.icelang};
+
         europa_nombre.add("España");
         europa_nombre.add("Francia");
         europa_nombre.add("Alemania");
         europa_nombre.add("Dinamarca");
         europa_nombre.add("Islandia");
-
-        int europa_bandera [] = {R.drawable.spain, R.drawable.france, R.drawable.germany, R.drawable.denmark, R.drawable.icelang};
 
         int n_bandera = numeroAleatorio("pais", europa_nombre.size());
 
@@ -53,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         img_bandera.setImageResource(europa_bandera[elegido.get(0)]);
+        respuesta.setText(europa_nombre.get(elegido.get(0)));
 
 
         int posicion_rb = numeroAleatorio("rb",3);
@@ -82,5 +89,30 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return numero;
+    }
+
+    public void Comprobar(View view){
+
+        String correcta = respuesta.getText().toString();
+        String seleccion = "";
+
+        if(rb1.isChecked() == true){
+           seleccion = rb1.getText().toString();
+        }
+        if(rb2.isChecked() == true){
+            seleccion = rb2.getText().toString();
+        }
+        if(rb3.isChecked() == true){
+            seleccion = rb3.getText().toString();
+        }
+
+
+        if(correcta.equals(seleccion)){
+            Toast.makeText(this, "Respuesta Correcta", Toast.LENGTH_SHORT).show();
+            Intent next = new Intent(this, MainActivity.class);
+            startActivity(next);
+        }else{
+            Toast.makeText(this, "NO!!! Prueba otra vez..", Toast.LENGTH_SHORT).show();
+        }
     }
 }
